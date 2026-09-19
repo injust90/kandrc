3a5c8ebea15b26e31a4c3484cc466bb39c5e6c99
JustinChou <justinjchou@gmail.com>
2026-09-19 07:54:28 -0700

    added some comments for legibility and future proofing

--- ch1/ex1-18.c
+++ ch1/ex1-18.c
@@ -15,17 +15,20 @@ int main()
     char store[MAXLINE];
 
     while((c=getchar()) != EOF) {
+        // If we see space or tab, store the space in a separate array
         if (c == ' ' || c == '\t') {
             store[i] = c;
             ++i;
             state = OUT;
         }
+        // If we see an newline reset the space counter and check to see if lines are blank or first line 
         else if (c == '\n') {
             i = 0;
             ++l;
             if (l == 1 && j != 0)
                 putchar('\n');
         }
+        // Else we see a word, and if we are "OUT" of the word then we need to store the space(s). Then reset the state.
         else {
             if (state == OUT) {
                 for (int k = 0; k < i; ++k) {
@@ -34,7 +37,8 @@ int main()
             }
             state = IN;
             l = 0;
-            if (j < 2) {
+            // Increment by one so that it is not the 1st line
+            if (j < 1) {
                 ++j;
             }
             putchar(c);
